916621388a555ddfefd878df968832328e906f0b
nagravi77 <nagravi.shankar@gmail.com>
2026-04-07 21:33:26 +1000

    task-1 code changes

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -35,3 +35,57 @@ def story_category(title):
     
     return None
 
+
+def main():
+    top_stories = fetch_top_story_ids()
+    fetched_stories =[]
+    category_counts = {cat: 0 for cat in CATEGORIES.keys() }
+
+    print("Fetching and  categorizing stories ...")
+
+    for category in CATEGORIES.keys():
+        print(f"\n Processiing Category: {category}")
+
+        for top_story in top_stories:
+            if category_counts[category] >= MAX_PER_CATEGORY:
+                break
+
+            story = fecth_story(top_story)
+            if not story:
+                continue
+
+            title = story.get("title","")
+            assigned_category = story_category(title)
+            if assigned_category != category:
+                continue
+
+            story_data ={
+                "post_id": story.get("id"),
+                "title": title,
+                "category": category,
+                "score": story.get("score", 0),
+                "num_comments": story.get("descendants", 0),
+                "author": story.get("by", "unknown"),
+                "collected_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+
+            fetched_stories.append(story_data)
+            category_counts[category] += 1
+
+        #Sleep After finishing each category
+        time.sleep(2)
+    
+    #create data folder if it doesnot exist
+    os.makedirs("data",exist_ok=True)
+
+    #File name with current date
+    filename =  f"data/trends_{datetime.now().strftime('%Y%m%d')}.json"
+
+    #save Json
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(fetched_stories, f, indent=4)
+
+    print(f"\n collected {len(fetched_stories)} stories. saved to {filename}")
+
+if __name__ == "__main__":
+    main()
